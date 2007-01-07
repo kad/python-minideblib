c377bca19d5f7510e21bb26265e14a5a2d11efa8
Alexandr D. Kanevskiy <kad@bifh.org>
2007-01-07 22:30:43 +0000

    add correct license notes

--- minideblib/DpkgChangelog.py
+++ minideblib/DpkgChangelog.py
@@ -1,7 +1,29 @@
 #!/usr/bin/python -tt
 # vim: sw=4 ts=4 expandtab ai
+#
+# DpkgChangelog.py
+#
+# This module implements parser for Debian changelog files
+#
+# Copyright (C) 2005,2006 Alexandr Kanevskiy
+#
+# Contact: Alexandr Kanevskiy <packages@bifh.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# version 2 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA
+#
 # $Id$
-# Copyright (C) 2005,2006 Alexandr Kanevskiy <packages@bifh.org>
 
 import re
 from minideblib import DpkgVersion
