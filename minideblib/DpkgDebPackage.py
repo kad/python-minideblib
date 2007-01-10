e1cded4ca2f30fa90f89eb0d6d7ae7759985f207
Alexandr D. Kanevskiy <kad@bifh.org>
2007-01-10 12:16:19 +0000

    Code cleanups and minor bugfixes

--- minideblib/DpkgDebPackage.py
+++ minideblib/DpkgDebPackage.py
@@ -36,38 +36,57 @@ import shutil
 import gzip 
 import tempfile
 import commands
+import errno
 
-from minideblib.DpkgControl import DpkgParagraph
-from minideblib.DpkgVersion import DpkgVersion
+from DpkgControl import DpkgParagraph
+from DpkgVersion import DpkgVersion
+from LoggableObject import LoggableObject
 
 class DpkgDebPackageException(Exception):
+    """General exception which could be raised by DpkgDebPackage"""
     def __init__(self, msg):
         Exception.__init__(self)
         self.msg = msg
+    def __str__(self):
+        return self.msg
     def __repr__(self):
         return self.msg
 
-class DpkgDebPackage:
+
+class DpkgDebPackage(LoggableObject):
     """This class represent complete information about Debian binary package"""
+
     def __init__(self, pkgfile=None):
         """ path -- path to .deb package """
         self.control = DpkgParagraph()
-        self.md5sums = []
-        self._md5files = None
+        self.md5sums = None
+        self.__md5files = None
         self.changes = None
         self.news = None
         self.files = None
-        self._raw_files = None
+        self.__raw_files = None
+        self.path = None
         if pkgfile:
-            self._path = pkgfile
             self.path = os.path.abspath(pkgfile)
+            if not os.path.isfile(self.path):
+                raise DpkgDebPackageException("Unable to locate file: %s" % self.path)
             self.load_control()
-       
-    def load(self, path, getfiles=True, getchanges='both'):
+
+
+    def load(self, path = None, getfiles=True, getchanges='both'):
         """ Loads .deb file for processing """
-        self._path = path
-        self.path = os.path.abspath(self._path)
-        self.load_control()
+        path_changed = False
+        if not path and not self.path:
+            raise DpkgDebPackageException("No deb file specified")
+        if path:
+            new_path = os.path.abspath(path)
+            if new_path != self.path:
+                self.path = new_path
+                path_changed = True
+        if not os.path.isfile(self.path):
+            raise DpkgDebPackageException("Unable to locate file: %s" % self.path)
+        if path_changed or not self.control:
+            self.load_control()
         if getchanges:
             self.load_changes(getchanges)
         if getfiles:
@@ -75,47 +94,58 @@ class DpkgDebPackage:
 
     def load_contents(self):
         """ Reads contents of .deb file into memory """
-        if self._path:
-            self._raw_files = self._list_contents()
-            if self._raw_files:
-                self.files = map(lambda fn: fn[5], self._raw_files)
+        if self.path and os.path.isfile(self.path):
+            self.__raw_files = self.__list_contents()
+            if self.__raw_files:
+                self.files = [ fname[5] for fname in self.__raw_files ]
         else: 
-            return False
+            raise DpkgDebPackageException("Unable to locate file: %s" % self.path)
 
     def load_changes(self, getchanges='both'):
         """ Reads changelog and/or news information into memory """
-        if self._path:
-            (news, changes) = self._extract_changes(getchanges)
+        if self.path and os.path.isfile(self.path):
+            (news, changes) = self.__extract_changes(getchanges)
             self.news = news
             self.changes = changes
-            return True
         else:
-            return False
+            raise DpkgDebPackageException("Unable to locate file: %s" % self.path)
 
     def load_control(self):
         """ Reads control information into memory """
-        if self._path:
+        if self.path and os.path.isfile(self.path):
             tempdir = self.__extract_control()
-            fh = open(os.path.join(tempdir,"control"),"r")
-            self.control.load(fh)
-            if not self._parse_md5sums(tempdir):
-                print "Can't parse md5sums"
-                #raise DpkgDebPackageException("Can't parse md5sums")
+            fhdl = open(os.path.join(tempdir,"control"),"r")
+            self.control = DpkgParagraph()
+            self.control.load(fhdl)
+            fhdl.close()
+            if not self.__parse_md5sums(tempdir):
+                self._logger.warning("Can't parse md5sums")
             else:
-                self._md5files = map(lambda x: x[1], self.md5sums)
+                self.__md5files = [ xsum[1] for xsum in self.md5sums ]
             shutil.rmtree(tempdir, True)
-            return True
         else:
-            return False
+            raise DpkgDebPackageException("Unable to locate file: %s" % self.path)
 
-    def _extract_changes(self, which, since_version=None):
+    def __extract_changes(self, which, since_version=None):
         '''Extract changelog entries, news or both from the package.
         If since_version is specified, only return entries later than the specified version.
         returns a sequence of Changes objects.'''
 
-        news_filenames = self.__changelog_variations('NEWS.Debian')
-        changelog_filenames = self.__changelog_variations('changelog.Debian')
-        changelog_filenames_native = self.__changelog_variations('changelog')
+        def changelog_variations(filename):
+            """Return list of all possible changelog/news locations"""
+            formats = ['usr/doc/*/%s.gz',
+                       'usr/share/doc/*/%s.gz',
+                       'usr/doc/*/%s',
+                       'usr/share/doc/*/%s',
+                       './usr/doc/*/%s.gz',
+                       './usr/share/doc/*/%s.gz',
+                       './usr/doc/*/%s',
+                       './usr/share/doc/*/%s']
+            return [ format % filename for format in formats ]
+        
+        news_filenames = changelog_variations('NEWS.Debian')
+        changelog_filenames = changelog_variations('changelog.Debian')
+        changelog_filenames_native = changelog_variations('changelog')
 
         filenames = []
         if which == 'both' or which == 'news':
@@ -128,7 +158,7 @@ class DpkgDebPackage:
 
         news = None
         for filename in news_filenames:
-            news = self._read_changelog(os.path.join(tempdir, filename),
+            news = self.__read_changelog(os.path.join(tempdir, filename),
                                        since_version)
             if news:
                 break
@@ -136,7 +166,7 @@ class DpkgDebPackage:
         changelog = None
         for batch in (changelog_filenames, changelog_filenames_native):
             for filename in batch:
-                changelog = self._read_changelog(os.path.join(tempdir, filename),
+                changelog = self.__read_changelog(os.path.join(tempdir, filename),
                                                 since_version)
                 if changelog:
                     break
@@ -148,13 +178,14 @@ class DpkgDebPackage:
         return (news, changelog)
 
     def __extract_control(self):
+        """Extracts content of control.tar.gz from .deb package"""
         try:
             tempdir = tempfile.mkdtemp(prefix='dpkgdebpackage')
         except AttributeError:
             tempdir = tempfile.mktemp()
             os.mkdir(tempdir)
 
-        extract_command = 'ar p %s control.tar.gz | tar zxf - -C %s 2>/dev/null' % (self._path, tempdir)
+        extract_command = 'ar p %s control.tar.gz | tar zxf - -C %s 2>/dev/null' % (self.path, tempdir)
 
         os.system(extract_command)
 
@@ -169,9 +200,9 @@ class DpkgDebPackage:
             os.mkdir(tempdir)
 
         extract_command = 'ar p %s data.tar.gz |tar zxf - -C %s %s 2>/dev/null' % (
-            self._path,
+            self.path,
             tempdir,
-            ' '.join(map(lambda x: "'%s'" % x, filenames))
+            ' '.join( [ "'%s'" % filen for filen in filenames ] )
             )
 
         # tar exits unsuccessfully if _any_ of the files we wanted
@@ -180,55 +211,58 @@ class DpkgDebPackage:
 
         return tempdir
     
-    def _parse_md5sums(self, tempdir):
+    def __parse_md5sums(self, tempdir):
         """Parses md5sums file from extracted control section of debian package"""
         path = os.path.join(tempdir,"md5sums")
         if not os.access(path, os.R_OK):
             print "Can't open file %s" % path
             return False
-        fh = open(path,"r")
-        for line in fh.readlines():
+        self.md5sums = []
+        fhdl = open(path,"r")
+        for line in fhdl.readlines():
             if line[33] != " ":
                 print "33 is not a space.\n %s" % line
                 # Something bad happend, unknown file format.
-                fh.close()
+                fhdl.close()
                 return False
-            ar = [line[:32].strip(), line[34:].strip()]
-            self.md5sums.append(ar)
-        fh.close()
+            argl = [ line[:32].strip(), line[34:].strip() ]
+            self.md5sums.append(argl)
+        fhdl.close()
         return True
 
-    def _list_contents(self):
-        (status, output) = commands.getstatusoutput("ar p %s data.tar.gz | tar ztvf -" % self._path)
+    def __list_contents(self):
+        """Returns filelist of data.tar.gz"""
+        (status, output) = commands.getstatusoutput("ar p %s data.tar.gz | tar ztvf -" % self.path)
         if status != 0:
             return []
-        files = map(lambda line: line.split(), output.splitlines())
+        files = [ line.split() for line in output.splitlines() ]
         return files
 
-    def _read_changelog(self, filename, since_version):
+    def __read_changelog(self, filename, since_version):
+        """Read changelog up to specified version"""
         changelog_header = re.compile('^\S+ \((?P<version>.*)\) .*;.*urgency=(?P<urgency>\w+).*')
         filenames = glob.glob(filename)
 
-        fd = None
+        fhdl = None
         for filename in filenames:
             try:
                 if filename.endswith('.gz'):
-                    fd = gzip.GzipFile(filename)
+                    fhdl = gzip.GzipFile(filename)
                 else:
-                    fd = open(filename)
+                    fhdl = open(filename)
                 break
-            except IOError, e:
-                if e.errno == errno.ENOENT:
+            except IOError, ioerr:
+                if ioerr.errno == errno.ENOENT:
                     pass
                 else:
                     raise
 
-        if not fd:
+        if not fhdl:
             return None
 
         changes = ''
         is_debian_changelog = 0
-        for line in fd.readlines():
+        for line in fhdl.readlines():
             match = changelog_header.match(line)
             if match:
                 is_debian_changelog = 1
@@ -237,20 +271,8 @@ class DpkgDebPackage:
                         break
             changes += line
 
+        fhdl.close()
         if not is_debian_changelog:
             return None
 
         return changes
-
-
-    def __changelog_variations(self, filename):
-        formats = ['usr/doc/*/%s.gz',
-                   'usr/share/doc/*/%s.gz',
-                   'usr/doc/*/%s',
-                   'usr/share/doc/*/%s',
-                   './usr/doc/*/%s.gz',
-                   './usr/share/doc/*/%s.gz',
-                   './usr/doc/*/%s',
-                   './usr/share/doc/*/%s']
-        return map(lambda format: format % filename, formats)
-
