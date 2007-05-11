2d4b0ac1242d759f4ede89b5f08de35cb022d51d
Alexandr D. Kanevskiy <kad@bifh.org>
2007-05-11 19:36:38 +0000

    make it use distutils/setuptools

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python -tt
+# -*- coding: UTF-8 -*-
+# vim: sw=4 ts=4 expandtab ai
+#
+# $Id$
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+version = "0.6.21.23"
+
+setup (name = "minideblib",
+    description = "Access to deb files and repositories",
+    version = version,
+    author = "Alexandr D. Kanevskiy",
+    author_email = "packages@bifh.org",
+    url = "http://bifh.org/wiki/python-minideblib",
+    license = "GPL",
+    packages = ['minideblib'],
+    long_description = "Python library for manipulating debian packages and repositories",
+    keywords = "python debian apt dpkg",
+    platforms="Python 2.3 and later.",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Operating System :: Unix",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Topic :: System :: Archiving :: Packaging",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+        ]
+    )
+
